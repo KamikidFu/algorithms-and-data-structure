a94e84724ff7b283b041d49a84baf01ce8bf5ddc
Kamikid <kidfu@live.com>
2019-05-26 11:36:35 +1200

    factorial method for js

--- JavaScript/Recursion.js
+++ JavaScript/Recursion.js
@@ -9,3 +9,16 @@ function sumRange(num) {
 
 console.log(sumRange(5));
 console.log(sumRange(0));
+
+function factorial(num) {
+    //Handle exception
+    if(num<=0) return 0;
+    //Base Case
+    if(num === 1) return 1;
+    //Next input
+    return num*factorial(num-1);
+}
+
+
+console.log(factorial(5));
+console.log(factorial(0));
