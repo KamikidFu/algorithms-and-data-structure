789de3ff68fc3121c51ac0e4ba3c10fc72beff04
Kamikid <kidfu@live.com>
2019-05-30 21:17:13 +1200

    Write selection sort

--- JavaScript/Sort.js
+++ JavaScript/Sort.js
@@ -1,3 +1,8 @@
+//A swap function using structuring
+const swap = (arr, index1, index2) => {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+};
+
 function BubbleSort(arr) {
     //In Bubble Sort, we are going to bubble
     //the max or min one to the end of array by swapping
@@ -5,14 +10,8 @@ function BubbleSort(arr) {
     //Third variable for swapping
     let swapTemp;
 
-    //A swap function using structuring
-    const swap = (arr, index1, index2) => {
-        [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
-    };
-
     //Swapping flag
     let isSwapped;
-
     //For outer loop, it is used to narrow down the total size of each iteration of array
     for(let outer = arr.length-1;outer>0;outer--){
         //Set the swapping flag to be false initially since there is no need to swap
@@ -45,11 +44,33 @@ function BubbleSort(arr) {
     return arr;
 }
 
-function SelectionSort() {
+function SelectionSort(arr) {
+    //The salient is a variable to record the lowest/highest value index
+    let salient;
+    //The outer loop will begin at 0 and go on
+    for(let outer = 0; outer<arr.length;outer++){
+        salient = outer;
+        //The inner loop will check through the rest of elements to pick up the salient
+        for(let inner = outer+1;inner<arr.length;inner++){
+            //For visualization
+            console.log("Current array:", arr);
 
+            //use a if statement to find the salient
+            if(arr[salient]>arr[inner]){
+                console.log("--->Find a new salient:", arr[inner]);
+                salient = inner;
+            }
+        }
+        //If the salient index has changed, swap these two value
+        if(outer !== salient) {
+            console.log("--->Swap:", arr[outer],arr[salient]);
+            swap(arr, outer, salient);
+        }
+    }
+    return arr;
 }
 
-function InsertionSort() {
+function InsertionSort(arr) {
 
 }
 
@@ -66,4 +87,4 @@ function RadixSort() {
 }
 
 let arr = [2,5,3,6,1,4];
-console.log(BubbleSort(arr));
+console.log(SelectionSort(arr));
